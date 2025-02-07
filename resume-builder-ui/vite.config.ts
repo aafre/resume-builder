c020045de5d9bba6c6a644c684c2aa7011b7e497
Amit Afre <amit.afre93@gmail.com>
2025-02-07 19:38:26 +0000

    UI packages

--- resume-builder-ui/vite.config.ts
+++ resume-builder-ui/vite.config.ts
@@ -1,8 +1,8 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "tailwindcss";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -17,5 +17,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
   }
 })
