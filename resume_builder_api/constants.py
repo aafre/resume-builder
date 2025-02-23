185acf651876c19a814fb175e7e0b87b5f7fb932
Amit Afre <amit.afre93@gmail.com>
2025-02-23 11:38:14 +0000

    Added a constants module

--- resume_builder_api/constants.py
+++ resume_builder_api/constants.py
@@ -1,5 +1,22 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
+TEMPLATES_DIR = BASE_DIR / "samples"  # For YAML Resume templates
 
-TEMPLATES_DIR = BASE_DIR / "samples"
+JINJA_TEMPLATES_DIR = (
+    BASE_DIR / "templates"
+)  # For Jinja templates - used for HTML to PDF conversion
+
+ICONS_DIR = (
+    BASE_DIR / "icons"
+)  # Needed for displaying icons into HTML templates (e.g., social media icons)
+
+OUTPUT_DIR = BASE_DIR / "output"  # Directory where PDFs and temp files will be saved
+
+
+# API constants
+CORS_ORIGINS = [
+    "http://localhost:3000",
+]
+
+MAX_REQUEST_SIZE = 10 * 1024 * 1024  # 10 MB
