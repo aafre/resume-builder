0baf11528826c1c8b5b5953cd4eb0a729056ac6f
Amit Afre <amit.afre93@gmail.com>
2026-02-03 20:56:56 +0000

    fix: remove unused useState import from LandingPage

--- resume-builder-ui/src/components/LandingPage.tsx
+++ resume-builder-ui/src/components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import SEOHead from "./SEOHead";
 import { generateSoftwareApplicationSchema, generateWebSiteSchema, generateFAQPageSchema, wrapInGraph } from "../utils/schemaGenerators";
